003e4497bf7f69d9e677981684ff4dfcb85eaf36
ShnaiderPavel <shnaiderpasha@gmail.com>
2016-03-07 16:59:50 +0300

    добавил 2 проекта

---
+++ DangerousLimousine/BackpackStreaming/TestClass.cpp
@@ -0,0 +1,9 @@
+//
+// Created by pavel on 07.03.16.
+//
+
+#include "TestClass.h"
+
+TestClass::TestClass() { }
+
+TestClass::~TestClass() { }
---
+++ DangerousLimousine/BackpackStreaming/TestClass.h
@@ -0,0 +1,17 @@
+//
+// Created by pavel on 07.03.16.
+//
+
+#ifndef DANGEROUSLIMOUSINE_TESTCLASS_H
+#define DANGEROUSLIMOUSINE_TESTCLASS_H
+
+
+class TestClass {
+public:
+    TestClass();
+
+    virtual ~TestClass();
+};
+
+
+#endif //DANGEROUSLIMOUSINE_TESTCLASS_H
